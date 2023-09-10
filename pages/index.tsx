f73f4c56b267c1602fe7759798ced097bd1fa7e3
Aruna Parameswaran <aruna.parame@gmail.com>
2023-09-10 14:29:10 -0500

    Made the code more reusable by abstracting information into info.json file

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,13 @@ import TypeWriter from '../src/TypeWriter';
 import { Button, Fab, Grid, Tooltip } from '@mui/material';
 import MuiLink from '@mui/material/Link';
 
+import info from '../public/info.json'
+
 export default function Home() {
   return (
     <div>
         <Head>
-            <title>Aruna's Portfolio</title>
+            <title>{info["Name"]}'s Portfolio</title>
         </Head>
         <Container maxWidth="lg">
             <Box
@@ -35,13 +37,13 @@ export default function Home() {
                     👋 Welcome! My name is
                 </Typography>
                 <Typography variant="h2" component="h1" gutterBottom sx={{ fontWeight: 'bold', m: 1 }}>
-                    Aruna Parameswaran
+                    {info["FullName"]}
                 </Typography>
                 <Typography variant="h4" component="h1" gutterBottom sx={{ m: 1 }}>
                     ... and I'm into
                 </Typography>
                 <Typography variant="h4" component="h1" gutterBottom sx={{ fontFamily: 'Monospace', m: 2 }} >
-                    <TypeWriter data={['Software Engineering', 'Designing scalable systems', 'Embedded Development', 'Frontend Engineering']}/>
+                    <TypeWriter data={info["InterestedIn"]}/>
                 </Typography>
                 
                 <Grid sx={{
@@ -62,17 +64,17 @@ export default function Home() {
                     alignItems: 'center',
                 }}>
                     <Grid item>
-                        <Link href="https://github.com/arunapa" target="_blank" >
+                        <Link href={info["Links"]["GitHub"]} target="_blank" >
                             <GitHubIcon fontSize="large"  />
                         </Link>
                     </Grid>
                     <Grid item>
-                        <Link href="https://www.linkedin.com/in/arunapa/" target="_blank" >
+                        <Link href={info["Links"]["LinkedIn"]} target="_blank" >
                             <LinkedInIcon fontSize="large" />
                         </Link>
                     </Grid>
                     <Grid item>
-                        <Link href="mailto:aruna.parame@gmail.com" target="_blank" >
+                        <Link href={info["Links"]["Email"]} target="_blank" >
                             <MailIcon fontSize="large" />
                         </Link>
                     </Grid>
--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import MuiLink from '@mui/material/Link';
 
+import info from '../public/info.json'
+
 export default function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center" sx={{bottom: 15, width: '100%', m: 4}}>
         Made with 💜
         <br></br>
       {'Copyright © '}
-      <MuiLink color="inherit" href="https://arunapa.dev/">
-        Aruna Parameswaran
+      <MuiLink color="inherit" href={info["url"]}>
+        {info["FullName"]}
       </MuiLink>{' '}
       {new Date().getFullYear()}.
     </Typography>
