2788d6fff147379312457c14b1845c6ee43fbeda
Aruna Parameswaran <aruna.parame@gmail.com>
2023-09-23 16:48:36 -0500

    Removed navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,6 @@ export default function Home() {
         <Head>
             <title>{info["FirstName"]}'s Portfolio</title>
         </Head>
-        <Navbar />
         <Container maxWidth="lg">
             <Box
                 sx={{
